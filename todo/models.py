c23c0349b5b66bff69c8724badf729d774fd6d43
Ali <parsa201909@gmail.com>
2023-09-02 20:02:26 +0330

    version_2

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.db.models import CheckConstraint, Q
 
 # Create your models here.
 class Todo(models.Model):
-  title= models.CharField(max_length=1000)
+  title= models.CharField(max_length=1000, null=False, blank=False)
+  priority = models.IntegerField(null=True, blank=True)  
+  due_date = models.DateField( default=timezone.now)
+
+
 
   def __str__(self) :
     return self.title
--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render, redirect
 from .models import Todo
 
+
 # Create your views here.
 
 
 def index(request):
     todo=Todo.objects.all()
     if request.method=='POST':
-        new_todo=Todo(
-            title=request.POST['title']
-        )
-        new_todo.save()
-        return redirect('/')
+        if request.POST['title']:
+
+        
+            if request.POST['priority'] and request.POST['date']:
+                new_todo=Todo(
+                    title=request.POST['title'],
+                    priority=request.POST['priority'],
+                    due_date=request.POST['date'],
+                )
+
+            elif request.POST['priority'] and not request.POST['date']:
+                new_todo=Todo(
+                    title=request.POST['title'],
+                    priority=request.POST['priority'],
+                
+                )
+
+            elif not request.POST['priority'] and  request.POST['date']:
+                new_todo=Todo(
+                    title=request.POST['title'],
+                    due_date=request.POST['date'],
+                
+                )
+            else:
+                new_todo=Todo(
+                    title=request.POST['title'],
+                
+                )
+            new_todo.save()
+            return redirect('/')
+        else:
+            pass
     
     context={
         'todos':todo,
